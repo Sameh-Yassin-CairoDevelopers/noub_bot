15e00e524aa1a947e79609ba468d6a7ec5231389
Sameh-Yassin-CairoDevelopers <165069856+Sameh-Yassin-CairoDevelopers@users.noreply.github.com>
2025-10-19 21:31:25 +0300

    Add files via upload

---
+++ js/state.js
@@ -0,0 +1,7 @@
+// This file holds the shared state of the application.
+// Modules can import this state to get access to the current user and profile.
+
+export const state = {
+    currentUser: null,
+    playerProfile: null,
+};
