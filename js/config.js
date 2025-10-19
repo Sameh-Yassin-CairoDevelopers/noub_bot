57ead9d8c13fa7d65127e9b2e3d4474d36c4da2f
Sameh-Yassin-CairoDevelopers <165069856+Sameh-Yassin-CairoDevelopers@users.noreply.github.com>
2025-10-19 11:37:43 -0700

    Restore configuration comments and keys in config.js

--- js/config.js
+++ js/config.js
@@ -2,7 +2,8 @@
 // All other modules will import the supabaseClient from here.
 
 const SUPABASE_URL = 'https://ryyiejjacfaxrfxeawcw.supabase.co';
-const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJ5eWllamphY2ZheHJmeGVhd2N3Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTk2Njc5ODcsImV4cCI6MjA3NTI2Mzk4N30.i-C9kLSS_Y_sLg0z9lYjQuPoD6OfhkCtgTJ_VJSVc4'; // This key seems to have a slight difference from the one I had, using your latest.
+const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJ5eWllamphY2ZheHJmeGVhd2N3Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTk2Njc5ODcsImV4cCI6MjA3NTI0Mzk4N30.4AwNsECeQnRRJtnoDldYjQuPoD6OfhkCtgTJ_VJSVc4'; // This key seems to have a slight difference from the one I had, using your latest.
 
 const { createClient } = supabase;
 export const supabaseClient = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
+
