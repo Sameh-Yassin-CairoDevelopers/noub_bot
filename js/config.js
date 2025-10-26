d2b1e43514c414eb7c2457f241bb59fa03d9e8fe
Sameh-Yassin-CairoDevelopers <165069856+Sameh-Yassin-CairoDevelopers@users.noreply.github.com>
2025-10-25 18:10:53 -0700

    Update config.js

--- js/config.js
+++ js/config.js
@@ -6,8 +6,9 @@
 
 // NOTE: Please replace these placeholders with your actual Supabase URL and ANON KEY
 const SUPABASE_URL = 'https://ryyiejjacfaxrfxeawcw.supabase.co'; 
-const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJ5eWllaWphY2ZheHJmeGVhd2N3Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MDQzNTQ2NzIsImV4cCI6MjAxOTkzMDY3Mn0.Rj5H9S9P4qf1qKq1tN3nE0YqA5g5A5g5g5A5g5A5g5A5g'; 
+const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJ5eWllamphY2ZheHJmeGVhd2N3Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTk2Njc5ODcsImV4cCI6MjA3NTI0Mzk4N30.4AwNsECeQnRRJtnoDldYjQuPoD6OfhkCtgTJ_VJSVc4'; 
 
 const supabaseClient = supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
 
 export { supabaseClient };
+
